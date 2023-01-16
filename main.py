f73df4c46db3353fde951dcac585867147e98d9c
RedTTG <redtonehair@gmail.com>
2023-01-17 01:30:51 +0200

    fix time

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from clementineremote import ClementineRemote
 c = ClementineRemote()
 p = pypresence.Presence(oauth.client_id)
 p.connect()
-s = time.time()-c.track_position
-e = s+c.current_track['length']
+s = 0
+e = 0
 def update(title: str = "song title", album: str = "album", current_track: int = 1, out_of_tracks: int = 2):
 	global s, e
 	p.update(
@@ -21,14 +21,15 @@ def update(title: str = "song title", album: str = "album", current_track: int =
 		party_size=[current_track, out_of_tracks]
 	)
 
-track = c.current_track['track_id']
+track = 't'
 tracks = int(input("how many tracks: "))
 
 while True:
 	if track != c.current_track['track_id']:
 		track = c.current_track['track_id']
-		s = time.time() - c.track_position
-		e = s + c.current_track['length']
+	left = c.current_track['length'] - c.track_position
+	s = time.time()
+	e = time.time() + left
 	update(c.current_track['title'], c.current_track['track_album'], c.current_track['track'], tracks)
 	try:
 		time.sleep(5)
